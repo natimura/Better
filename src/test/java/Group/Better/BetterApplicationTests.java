f4ff2e184b2a5665e5f61e7da53d8c50f2ee2183
natimura <natimura10@gmail.com>
2023-06-27 18:53:07 +0900

    Initial commit

---
+++ src/test/java/Group/Better/BetterApplicationTests.java
@@ -0,0 +1,13 @@
+package Group.Better;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
