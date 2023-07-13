be00174dc9f6dc9c967c744bcba6f8e602876f2e
natimura <natimura10@gmail.com>
2023-07-13 19:47:04 +0900

    一部修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS posts (
 CREATE TABLE IF NOT EXISTS choices (
     id SERIAL NOT NULL PRIMARY KEY,
     choice_content VARCHAR(256) NOT NULL,
-　　 vote_count BIGINT,
+    vote_count BIGINT,
     post_id BIGINT NOT NULL,
     FOREIGN KEY (post_id) REFERENCES posts (id)
 );
