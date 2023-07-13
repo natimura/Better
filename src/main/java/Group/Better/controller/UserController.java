b89423f39343e109d1e519fa2aa00f8c50ad5fdc
natimura <natimura10@gmail.com>
2023-07-13 20:06:43 +0900

    デプロイエラー検証

--- src/main/java/Group/Better/controller/UserController.java
+++ src/main/java/Group/Better/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
 
     @GetMapping("/signUp")
     public String signUpCreation(@ModelAttribute("user") User user){
-        return "/signUp";
+        return "signUp";
     }
 
     @PostMapping("/signUp")
